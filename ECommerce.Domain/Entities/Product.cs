8d316cb629235eb126f6bf2e91e513539d8ab68f
Bekmurod <113014649+Bekmurod21@users.noreply.github.com>
2023-02-14 15:13:36 +0500

    Product completed

--- ECommerce.Domain/Entities/Product.cs
+++ ECommerce.Domain/Entities/Product.cs
@@ -1,14 +1,14 @@
-﻿namespace ECommerce.Domain.Entities
+﻿using ECommerce.Domain.Commons;
+
+namespace ECommerce.Domain.Entities
 {
-    public class Product
+    public class Product :Auditable
     {
-        public long Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public bool Can_Deleiver { get; set; }
+        public bool CanDeleiver { get; set; }
         public decimal Price { get; set; }
-        public long Category_Id { get; set;}
-        public DateTime Created_At { get; set; }
-        public DateTime Updated_At { get;set; }
+        public long CategoryId { get; set;}
+        
     }
 }

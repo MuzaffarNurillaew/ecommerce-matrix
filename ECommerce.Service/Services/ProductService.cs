fa54780ffeea456e36857ee87f3e6eada5610991
unknown <azimochilov226@gmail.com>
2023-02-14 23:12:48 +0300

    Added Product Service

--- ECommerce.Service/Services/ProductService.cs
+++ ECommerce.Service/Services/ProductService.cs
@@ -1,6 +1,36 @@
-﻿namespace ECommerce.Service.Services
+﻿using ECommerce.Data.Repositories;
+using ECommerce.Domain.Entities;
+using ECommerce.Service.Helpers;
+using ECommerce.Service.Interfaces;
+
+namespace ECommerce.Service.Services
 {
-    public class ProductService
+    public class ProductService : IProductService
     {
+        private readonly Repository<Product> genericRepository = new GenericRepository<Product>();
+        Task<Response<Product>> IProductService.AddAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Response<List<Product>>> IProductService.GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Response<Product>> IProductService.GetByIdAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Response<Product>> IProductService.GetByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<Response<Product>> IProductService.UpdateAsync(long id, Product product)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

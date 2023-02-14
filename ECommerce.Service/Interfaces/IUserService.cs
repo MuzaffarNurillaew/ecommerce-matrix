0a2d1746020eba7ff187760c682bfea6bf334607
Muzaffarov03 <firdavsmuzaffarov03@gmail.com>
2023-02-14 15:46:24 +0500

    User Interface was finished

--- ECommerce.Service/Interfaces/IUserService.cs
+++ ECommerce.Service/Interfaces/IUserService.cs
@@ -1,6 +1,23 @@
-﻿namespace ECommerce.Service.Interfaces
+﻿using ECommerce.Domain.Entities;
+using ECommerce.Service.Helpers;
+
+namespace ECommerce.Service.Interfaces
 {
     public interface IUserService
     {
+        //  CRUD    methods
+
+        // creating new product
+        public Task<Response<User>> CreateAsync(User user);
+        // changing product information
+        public Task<Response<User>> UpdateAsync(long id, User user);
+        // finding by id
+        public Task<Response<User>> GetByIdAsync(long id);
+        // finding by name
+        public Task<Response<User>> GetByNameAsync(string name);
+        // taking all information 
+        public Task<Response<List<User>>> GetAllAsync();
+        // Deleting user 
+        public Task<Response<bool>> DeleteAsync(long id);
     }
 }

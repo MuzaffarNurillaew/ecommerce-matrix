12f1b61a2b0555c243b0904037b5068cbdf86229
Muzaffar Nurillayev <102397633+MuzaffarNurillaew@users.noreply.github.com>
2023-02-14 16:14:02 +0500

    OrderService class

---
+++ ECommerce.Service/Services/OrderService.cs
@@ -0,0 +1,34 @@
+﻿using ECommerce.Domain.Entities;
+using ECommerce.Service.Helpers;
+using ECommerce.Service.Interfaces;
+
+namespace ECommerce.Service.Services
+{
+    public class OrderService : IOrderService
+    {
+        public Response<Task<Order>> AddAsync(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response<bool> DeleteAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response<List<Order>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response<Order> GetAsync(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Response<Order> UpdateAsync(long id, Order order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

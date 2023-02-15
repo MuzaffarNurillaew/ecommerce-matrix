ed1425810fa44154406dea534e84154c87c97750
Bekmurod <113014649+Bekmurod21@users.noreply.github.com>
2023-02-16 00:32:43 +0500

    AdminUI complited

--- ECommerce.Presentation/AdminUI/AdminUI.cs
+++ ECommerce.Presentation/AdminUI/AdminUI.cs
@@ -1,6 +1,8 @@
 ﻿
+using ECommerce.Domain.Entities;
 using ECommerce.Service.Interfaces;
 using ECommerce.Service.Services;
+using System.Reflection;
 
 namespace ECommerce.Presentation.AdminUI
 {
@@ -10,6 +12,7 @@ namespace ECommerce.Presentation.AdminUI
         private IUserService userService = new UserService();
         public async Task Admin()
         {
+            AdminMenu:
             Console.WriteLine("         Main Menu   ");
             Console.WriteLine("1.Search User");
             Console.WriteLine("2.Get All Users");
@@ -21,10 +24,17 @@ namespace ECommerce.Presentation.AdminUI
                 if (number == 1)
                 {
                     SearchAsync();
+                    goto AdminMenu;
                 }
                 else if (number == 2)
                 {
                     GetAsync();
+                    goto AdminMenu;
+                }
+                else if(number == 3)
+                {
+                    DeleteUserAsync();
+                    goto AdminMenu;
                 }
             }
         }
@@ -48,7 +58,7 @@ namespace ECommerce.Presentation.AdminUI
                     {
                         Console.WriteLine(model.Message);
                     }
-                    Console.WriteLine(value: $"Name: {model.Result.FirstName} LastName: {model.Result.LastName} UserName: {model.Result.Username}");
+                    Console.WriteLine($"Id: {model.Result.Id} Name: {model.Result.FirstName} LastName: {model.Result.LastName} UserName: {model.Result.Username}");
                     Console.WriteLine($"Email: {model.Result.Email} Phone Number: {model.Result.PhoneNumber} Money: {model.Result.AvailableMoney}");
 
                     Console.ReadKey();
@@ -63,7 +73,7 @@ namespace ECommerce.Presentation.AdminUI
                     {
                         Console.WriteLine(model.Message);
                     }
-                    Console.WriteLine(value: $"Name: {model.Result.FirstName} LastName: {model.Result.LastName} UserName: {model.Result.Username}");
+                    Console.WriteLine($"Id: {model.Result.Id} Name: {model.Result.FirstName} LastName: {model.Result.LastName} UserName: {model.Result.Username}");
                     Console.WriteLine($"Email: {model.Result.Email} Phone Number: {model.Result.PhoneNumber} Money: {model.Result.AvailableMoney}");
 
                     Console.ReadKey();
@@ -74,9 +84,31 @@ namespace ECommerce.Presentation.AdminUI
         public async Task GetAsync()
         {
             var model = await userService.GetAllAsync();
-            
+            foreach (var item in model.Result)
+            {
+                Console.WriteLine("====================================================================================");
+                Console.WriteLine($"Id: {item.Id} Name: {item.FirstName} LastName: {item.LastName} UserName: {item.Username}");
+                Console.WriteLine($"Email: {item.Email} Phone Number: {item.PhoneNumber} Money: {item.AvailableMoney}");
+            }
+            Console.ReadKey();
 
+        }
+        public async Task DeleteUserAsync()
+        {
+            Console.Write("Enter the id of the User you want to delete: ");
+            int deleteid = int.Parse(Console.ReadLine());
+            var model = userService.DeleteAsync(deleteid);
+            if (model.Result.Result == true)
+            {
+                Console.WriteLine(model.Result.Message);
+            }
+            else
+            {
+                Console.WriteLine("User is not found");
+            }
 
+            Console.Write("Press any key to return main menu.");
+            Console.ReadKey(true);
         }
     }
 }

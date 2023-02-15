19fbc02f6fe946cd65e7360d87bafc5f2a0e122f
Muzaffar Nurillayev <102397633+MuzaffarNurillaew@users.noreply.github.com>
2023-02-15 11:56:47 +0500

    OwnerId

--- ECommerce.Domain/Entities/Product.cs
+++ ECommerce.Domain/Entities/Product.cs
@@ -1,11 +1,12 @@
 ﻿using ECommerce.Domain.Commons;
+using ECommerce.Domain.Enums;
 
 namespace ECommerce.Domain.Entities
 {
     public class Product : Auditable
     {
-        public long CategoryId { get; set;}
         public long OwnerId { get; set; }
+        public ProductCategory Category { get; set;}
         public string Name { get; set; }
         public decimal Price { get; set; }
         public bool CanDeliver { get; set; }
